c31c36bd24f3ccdbcdc042958d60754e8cdb4eee
lakshyarawat1 <lakshya.paramount@gmail.com>
2025-01-03 17:18:02 +0530

    [ #2 Addition - Added basic models and db connection ]

---
+++ spring_shops/src/main/java/com/project/spring_shops/SpringShopsApplication.java
@@ -0,0 +1,13 @@
+package com.project.spring_shops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShopsApplication.class, args);
+	}
+
+}
